ec9b4a996eeca003d16e0ed43e7546f8f08c6dd9
wangyu <wangyu@houyicloud.com>
2019-04-29 19:37:21 +0800

    chain

--- Example/HYCloudChain/HYViewController.m
+++ Example/HYCloudChain/HYViewController.m
@@ -10,17 +10,18 @@
 #import "HYChainView.h"
 #import <HYBaseModule/HYBaseModule.h>
 #import <HYDefine/HYDefine.h>
-
+#define minScale  1
+#define maxScale  2
 @interface HYViewController ()
 
-
+@property(nonatomic,strong)UIScrollView *scrollView;
 @end
 
 @implementation HYViewController
 
 - (void)viewDidLoad
 {
-    [UIApplication sharedApplication] set
+    
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     [self initData];
@@ -34,22 +35,29 @@
 //    }];
     
     
-    HYChainView *chainView = [[HYChainView alloc] initWithFrame:CGRectMake(0, 0, kScreenHeight, kScreenWidth)];
-    UIScrollView *scroll = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
-    [self.view addSubview:scroll];
-    scroll.scrollEnabled = YES;
-    scroll.userInteractionEnabled = YES;
-    scroll.contentSize = CGSizeMake(2000, 2000);
-    scroll.maximumZoomScale = 2;
-    [scroll addSubview:chainView];
+    HYChainView *chainView = [[HYChainView alloc] initWithFrame:CGRectMake(0, 0, 1000, 1000)];
+    _scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
+    [self.view addSubview:_scrollView];
+    _scrollView.scrollEnabled = YES;
+    _scrollView.userInteractionEnabled = YES;
+    _scrollView.contentSize = CGSizeMake(1000, 1000);
+    _scrollView.maximumZoomScale = 2;
+    [_scrollView addSubview:chainView];
+
+    
+    
+    [_scrollView setMinimumZoomScale:minScale];
+    [_scrollView setMaximumZoomScale:maxScale];
+    [_scrollView setZoomScale:minScale animated:YES];
+  
     
 }
 
 - (void)initData {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"chainjson" ofType:@"json"];
-    NSData *data = [[NSData alloc] initWithContentsOfFile:path];
+//    NSString *path = [[NSBundle mainBundle] pathForResource:@"chainjson" ofType:@"json"];
+//    NSData *data = [[NSData alloc] initWithContentsOfFile:path];
     
-    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+//    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
     
 //    _nodeDataArray = [jsonDict[@"nodeDataArray"] mutableCopy];
 }
@@ -62,8 +70,40 @@
 
 
 
+-(void)viewWillAppear:(BOOL)animated{
+    NSNumber *orientationUnknown = [NSNumber numberWithInt:UIInterfaceOrientationUnknown];
+    [[UIDevice currentDevice] setValue:orientationUnknown forKey:@"orientation"];
+    
+    NSNumber *orientationTarget = [NSNumber numberWithInt:UIInterfaceOrientationLandscapeLeft];
+    [[UIDevice currentDevice] setValue:orientationTarget forKey:@"orientation"];
+}
 
 
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return scrollView;
+}
 
+
+// called before the scroll view begins zooming its content缩放开始的时候调用
+- (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(nullable UIView *)view NS_AVAILABLE_IOS(3_2){
+    NSLog(@"%s",__func__);
+}
+
+// scale between minimum and maximum. called after any 'bounce' animations缩放完毕的时候调用。
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale
+
+{
+    //把当前的缩放比例设进ZoomScale，以便下次缩放时实在现有的比例的基础上
+    
+    [_scrollView setZoomScale:scale animated:NO];
+}
+
+// 缩放时调用
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView{
+    // 可以实时监测缩放比例
+    NSLog(@"......scale is %f",scrollView.zoomScale);
+    
+}
 
 @end
